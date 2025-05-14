891978edaf74024eb4a8f52515d79c99c59ff460
Daniel Ferreira <danferreiradev@gmail.com>
2025-05-13 23:13:31 -0300

    merge torrent_file into metadata file

--- internal/metadata/metadata.go
+++ internal/metadata/metadata.go
@@ -1,7 +1,15 @@
 package metadata
 
 import (
+	"bytes"
+	"crypto/sha1"
 	"net/url"
+	"os"
+	"path/filepath"
+	"slices"
+	"strings"
+
+	"github.com/jackpal/bencode-go"
 )
 
 type Metadata struct {
@@ -20,6 +28,104 @@ type Info struct {
 type FileInfo struct {
 	Path   string
 	Length int
+}
+
+type torrentFile struct {
+	Announce string          `bencode:"announce"`
+	Info     torrentFileInfo `bencode:"info"`
+}
+
+type torrentFileInfo struct {
+	Name        string                `bencode:"name"`
+	Pieces      string                `bencode:"pieces"`
+	PieceLength int                   `bencode:"piece length"`
+	Length      int                   `bencode:"length"`
+	Files       []torrentFileInfoFile `bencode:"files"`
+}
+
+type torrentFileInfoFile struct {
+	Path   []string `bencode:"path"`
+	Length int      `bencode:"length"`
+}
+
+func Parse(path string) (*Metadata, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	tf := torrentFile{}
+	if err = bencode.Unmarshal(file, &tf); err != nil {
+		return nil, err
+	}
+
+	announceURL, err := url.Parse(tf.Announce)
+	if err != nil {
+		return nil, err
+	}
+
+	var files []FileInfo
+
+	if len(tf.Info.Files) > 0 {
+		for _, file := range tf.Info.Files {
+			path := filepath.Join(tf.Info.Name, strings.Join(file.Path, "/"))
+			fileInfo := FileInfo{
+				Path:   path,
+				Length: file.Length,
+			}
+
+			files = append(files, fileInfo)
+		}
+	} else {
+		fileInfo := FileInfo{
+			Path:   tf.Info.Name,
+			Length: tf.Info.Length,
+		}
+
+		files = append(files, fileInfo)
+	}
+
+	chunks := slices.Collect(slices.Chunk([]byte(tf.Info.Pieces), 20))
+	pieces := make([][20]byte, 0, len(chunks))
+
+	for _, chunk := range chunks {
+		var arr [20]byte
+		copy(arr[:], chunk)
+		pieces = append(pieces, arr)
+	}
+
+	dict := map[string]interface{}{
+		"name":         tf.Info.Name,
+		"pieces":       tf.Info.Pieces,
+		"piece length": tf.Info.PieceLength,
+	}
+
+	// If it's single-file, we add "length"
+	// If it's multi-file, we add "files"
+	if len(tf.Info.Files) == 0 {
+		dict["length"] = tf.Info.Length
+	} else {
+		dict["files"] = tf.Info.Files
+	}
+
+	var buf bytes.Buffer
+	if err = bencode.Marshal(&buf, dict); err != nil {
+		return nil, err
+	}
+
+	hash := sha1.Sum(buf.Bytes())
+
+	return &Metadata{
+		Announce: announceURL,
+		Info: Info{
+			Name:        tf.Info.Name,
+			Pieces:      pieces,
+			PieceLength: tf.Info.PieceLength,
+			Files:       files,
+			InfoHash:    hash,
+		},
+	}, nil
 }
 
 func (t *Info) TotalLength() int {
--- internal/metadata/metadata_test.go
+++ internal/metadata/metadata_test.go
@@ -6,6 +6,38 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestParseSingleFileTorrent(t *testing.T) {
+	metadata, err := Parse("testdata/single_file.torrent")
+
+	assert.Nil(t, err)
+	assert.Equal(t, "http://localhost:8000/announce", metadata.Announce.String())
+	assert.Equal(t, "file_1.txt", metadata.Info.Name)
+	assert.Equal(t, 1, len(metadata.Info.Files))
+	assert.Equal(t, 405, len(metadata.Info.Pieces))
+	assert.Equal(t, 32768, metadata.Info.PieceLength)
+	assert.Equal(t, "file_1.txt", metadata.Info.Files[0].Path)
+
+	var expectedHash = [20]byte{0xba, 0x3e, 0xfa, 0xe8, 0xc4, 0xd8, 0x4a, 0xad, 0xbf, 0xca, 0xc7, 0x56, 0xf7, 0xee, 0xb5, 0x6b, 0x9e, 0x1f, 0xc2, 0x21}
+	assert.Equal(t, expectedHash, metadata.Info.InfoHash)
+}
+
+func TestParseMultiFileTorrent(t *testing.T) {
+	metadata, err := Parse("testdata/multi_files.torrent")
+
+	assert.Nil(t, err)
+	assert.Equal(t, "http://localhost:8000/announce", metadata.Announce.String())
+	assert.Equal(t, "files", metadata.Info.Name)
+	assert.Equal(t, 3, len(metadata.Info.Files))
+	assert.Equal(t, 658, len(metadata.Info.Pieces))
+	assert.Equal(t, 32768, metadata.Info.PieceLength)
+	assert.Equal(t, "files/file_1.txt", metadata.Info.Files[0].Path)
+	assert.Equal(t, "files/file_2.txt", metadata.Info.Files[1].Path)
+	assert.Equal(t, "files/file_3.txt", metadata.Info.Files[2].Path)
+
+	var expectedHash = [20]byte{0x1c, 0x78, 0xe7, 0x26, 0x0d, 0x0b, 0xcf, 0x7f, 0xa2, 0x72, 0xea, 0xd1, 0xef, 0xc8, 0x47, 0x9c, 0xc4, 0xaa, 0x84, 0xf0}
+	assert.Equal(t, expectedHash, metadata.Info.InfoHash)
+}
+
 func TestTotalLength(t *testing.T) {
 	metadata := Metadata{
 		Info: Info{
